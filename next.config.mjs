da858e4bd4d5f500b240f01422200804da075996
metasal <54984459+metasal1@users.noreply.github.com>
2024-12-27 20:46:59 +1100

    removed headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline';
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https:;
-              frame-src 'self' https://embedded.passkeys.foundation;
-              frame-ancestors 'self';
-              font-src 'self';
-              connect-src 'self' https://api.yourdomain.com;
-            `.replace(/\s{2,}/g, ' ').trim()
-                    }
-                ]
-            }
-        ]
-    }
-}
+const nextConfig = {};
 
 export default nextConfig;
